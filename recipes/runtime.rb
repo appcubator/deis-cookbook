ae96ef79a14551962bf9c4be118328b2c50704ab
Karan Sikka <kssworld93@gmail.com>
2014-02-03 12:51:55 -0500

    chefff

--- recipes/runtime.rb
+++ recipes/runtime.rb
@@ -135,20 +135,18 @@ end # formations.each
 
 
 # remove old slug dirs
-ruby_block 'old slug removal' do
-  block do
-    slug_root = node.deis.runtime.slug_dir
-    Dir.entries(slug_root).each do |f|
-      next if f == '.'
-      next if f == '..'
+slug_root = node.deis.runtime.slug_dir
+if File.directory?(slug_root)
+  Dir.entries(slug_root).each do |f|
+    next if f == '.'
+    next if f == '..'
 
-      slug_dir = "#{slug_root}/#{f}"
+    slug_dir = "#{slug_root}/#{f}"
 
-      directory slug_dir do
-        action :delete
-        recursive true
-        not_if { active_slug_paths.include? f }
-      end
+    directory slug_dir do
+      action :delete
+      recursive true
+      not_if { active_slug_paths.include? f }
     end
   end
 end
